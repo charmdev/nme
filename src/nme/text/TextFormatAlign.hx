18948d640e93ee184aa726d056dafa8637a30c57
Zaphod <zaphy@yandex.ru>
2019-08-28 14:13:59 +0300

    change TextFormatAlign from abstract enum to class with static consts in order to make it friendly to hot reloading

--- src/nme/text/TextFormatAlign.hx
+++ src/nme/text/TextFormatAlign.hx
@@ -1,13 +1,24 @@
 package nme.text;
 #if (!flash)
 
-@:enum
+/*@:enum
 abstract TextFormatAlign(String) from String to String
 {
    var LEFT = "left";
    var RIGHT = "right";
    var CENTER = "center";
    var JUSTIFY = "justify";
+}*/
+
+class TextFormatAlign 
+{
+	public static inline var LEFT = "left";
+	public static inline var RIGHT = "right";
+	public static inline var CENTER = "center";
+	public static inline var JUSTIFY = "justify";
+	
+	public static inline var START = "left";
+	public static inline var END = "right";
 }
 
 #else
